bca8220362094011dd7f08c5769a11aea763feda
San Yan <williamchen014@gmail.com>
2026-01-21 16:01:43 -0500

    Update API key placeholder in scrape_comments.sh
    Replaced the API key in the scrape_comments.sh script with a placeholder.

--- youtube/scraping/scrape_comments.sh
+++ youtube/scraping/scrape_comments.sh
@@ -8,4 +8,4 @@ module load python3/3.13.8
 
 pip install --user youtube-comment-downloader
 
-python youtube_scraping.py --api-key AIzaSyDfkzqybna2xOe5L81RRHTRNYBOnTlw62k --skip-dates --resume
+python youtube_scraping.py --api-key  #### --skip-dates --resume
